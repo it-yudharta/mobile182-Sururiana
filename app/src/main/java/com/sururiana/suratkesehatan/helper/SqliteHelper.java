7ca4add35d74ca22abd6dfa8dec9c4e776a22d79
Sururiana <44032062+Sururiana@users.noreply.github.com>
2019-06-01 14:25:23 +0700

    Create SqliteHelper.java

---
+++ app/src/main/java/com/sururiana/suratkesehatan/helper/SqliteHelper.java
@@ -0,0 +1,27 @@
+package com.sururiana.suratkesehatan.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Suratkesehatan";
+    private static final int DATABASE_VERSION = 1;
+
+    public SqliteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        //TODO auto-generated construcktor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //TODO Auto-generated method stub
+        db.execSQL(
+                "CREATE TABLE kesehatan (no_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, status TEXT," +
+                        "nama TEXT, berat DOUBLE, tensi DOUBLE, keterangan TEXT, tanggal DATE DEFAULT CURRENT_DATE);"
+        );
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS kesehatan");
+    }
+}
